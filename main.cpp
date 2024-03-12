364a5d964b2ab74c6e32dcc166f2562ebb5898ac
mozhong123 <1254620366@qq.com>
2024-03-12 21:02:03 +0800

    新增cmakelists，新增部分初始化内容：货物类及部分注释

---
+++ main.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int n = 200;
+const int robot_num = 10;
+const int berth_num = 10;
+const int N = 210;
+
+struct Robot  // 10个机器人
+{
+    int x, y, goods;
+    int status;
+    int mbx, mby;
+    Robot() {}
+    Robot(int startX, int startY) {
+        x = startX;
+        y = startY;
+    }
+}robot[robot_num + 10];
+
+struct Berth  // 10个泊位
+{
+    int x;
+    int y;
+    int transport_time;
+    int loading_speed;
+    Berth(){}
+    Berth(int x, int y, int transport_time, int loading_speed) {
+        this -> x = x;
+        this -> y = y;
+        this -> transport_time = transport_time;
+        this -> loading_speed = loading_speed;
+    }
+}berth[berth_num + 10];
+
+struct Boat // 5艘船
+{
+    int num, pos, status;
+}boat[10];
+
+struct Good  // 物品
+{
+    int x;
+    int y;
+    int val;
+    int zhen;  // 帧数
+    Good(){}
+    Good(int x, int y, int val, int zhen) {
+        this -> x = x;
+        this -> y = y;
+        this -> val = val;
+        this -> zhen= zhen;
+    }
+};
+
+int money, boat_capacity, id;
+char ch[N][N];  // 地图
+int gds[N][N];  // 官方给的，但似乎没用
+vector <Good> good;  // 货物
+void Init()  // 初始化地图与泊位
+{
+    for(int i = 1; i <= n; i ++)
+        scanf("%s", ch[i] + 1);
+    for(int i = 0; i < berth_num; i ++)
+    {
+        int id;
+        scanf("%d", &id);
+        scanf("%d%d%d%d", &berth[id].x, &berth[id].y, &berth[id].transport_time, &berth[id].loading_speed);
+    }
+    scanf("%d", &boat_capacity);
+    char okk[100];
+    scanf("%s", okk);
+    printf("OK\n");
+    fflush(stdout);
+}
+
+int Input(int zhen)  // 每帧交互
+{
+    scanf("%d%d", &id, &money);
+    int num;
+    scanf("%d", &num);
+    for(int i = 1; i <= num; i ++)
+    {
+        int x, y, val;
+        scanf("%d%d%d", &x, &y, &val);
+        good.push_back(Good(x, y, val, zhen));
+    }
+    for(int i = 0; i < robot_num; i ++)
+    {
+        int sts;
+        scanf("%d%d%d%d", &robot[i].goods, &robot[i].x, &robot[i].y, &sts);
+    }
+    for(int i = 0; i < 5; i ++)
+        scanf("%d%d\n", &boat[i].status, &boat[i].pos);
+    char okk[100];
+    scanf("%s", okk);
+    return id;
+}
+
+int main()
+{
+    Init();
+    for(int zhen = 1; zhen <= 15000; zhen ++)
+    {
+        int id = Input(zhen);
+        for(int i = 0; i < robot_num; i ++)
+            printf("move %d %d\n", i, rand() % 4);
+        puts("OK");
+        fflush(stdout);
+    }
+
+    return 0;
+}
